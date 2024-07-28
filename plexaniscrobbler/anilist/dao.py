3dac55ad632ba5e066244ba6a43370e7735d82ea
twissell- <dmaggioesne@gmail.com>
2024-07-28 02:45:29 +0200

    Removed Authorization header from get list requests

--- plexaniscrobbler/anilist/dao.py
+++ plexaniscrobbler/anilist/dao.py
@@ -72,6 +72,10 @@ class Anilist(metaclass=Singleton):
                 "query": query.LIST_BY_USERNAME_AND_STATUS,
                 "variables": {"username": username, "status": status},
             },
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
         )
 
         entries = (
